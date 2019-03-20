08cb1b619cc1c4f7497f354f5346af86b6812ad1
TangBean <984354469@qq.com>
2019-03-20 08:04:03 +0800

    AC-PurpleBook-UVA-11059-Maximum Product

--- src/main/java/org/alg/purplebook/ch7/example2/Main.java
+++ src/main/java/org/alg/purplebook/ch7/example2/Main.java
@@ -2,6 +2,17 @@ package org.alg.purplebook.ch7.example2;
 
 import java.util.Scanner;
 
+/**
+ * 这道题的数据量很小，所以遍历所有的可能情况也可以，不过这种做法不是很好。
+ *
+ * 较好做法：DP 思想，在遍历 nums[i] 的过程中维护两个状态：
+ * - pos: 以 nums[i] 为结尾的序列的最大正值
+ * - neg: 以 nums[i] 为结尾的序列的最小负值
+ * 这样做是因为：
+ * - 如果当前 nums[i] > 0，那么就直接 pos = pos * nums[i] 和 neg = neg * nums[i] 就行
+ * - 如果当前 nums[i] < 0，那么则需要交换 pos 和 neg 的值
+ * - 如果当前 nums[i] == 0，需要把 pos 和 neg 设为 0
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -30,7 +41,7 @@ public class Main {
                     pos = nums[i];
                 }
             } else {
-                long tmp = pos + nums[i];
+                long tmp = pos * nums[i];
                 pos = neg * nums[i];
                 neg = tmp;
                 if (neg == 0) {
