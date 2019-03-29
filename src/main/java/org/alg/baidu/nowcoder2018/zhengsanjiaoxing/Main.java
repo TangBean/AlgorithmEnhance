9fcbdf6bb37db10a68639201bff2cc9ca5ff46de
TangBean <984354469@qq.com>
2019-03-29 08:11:41 +0800

    baidu: 正三角形的顶点位置_unfinished

--- src/main/java/org/alg/baidu/nowcoder2018/zhengsanjiaoxing/Main.java
+++ src/main/java/org/alg/baidu/nowcoder2018/zhengsanjiaoxing/Main.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        double[] input = new double[4];
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < 4; j++) {
+                input[j] = in.nextDouble();
+            }
+            double[] output = getThirdPoints(input);
+            System.out.printf("%.2f %.2f %.2f %.2f", output[0], output[1], output[2], output[3]);
+        }
+    }
+
+    private static double[] getThirdPoints(double[] input) {
 
     }
 }
