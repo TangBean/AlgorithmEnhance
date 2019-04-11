9998fe08c259fdbbf50b1edf9f7d5471a671ed0e
TangBean <984354469@qq.com>
2019-04-11 08:02:59 +0800

    luogu-p1-ch4 P1046 陶陶摘苹果_finished & P1047 校门外的树_unfinished

--- src/main/java/org/luogu/part1/ch4/p1046/Main.java
+++ src/main/java/org/luogu/part1/ch4/p1046/Main.java
@@ -1,4 +1,21 @@
 package org.luogu.part1.ch4.p1046;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] treeHeight = new int[10];
+        for (int i = 0; i < 10; i++) {
+            treeHeight[i] = in.nextInt();
+        }
+        int taoHeight = 30 + in.nextInt();
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            if (taoHeight >= treeHeight[i]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
--- src/main/java/org/luogu/part1/ch4/p1047/Main.java
+++ src/main/java/org/luogu/part1/ch4/p1047/Main.java
@@ -1,4 +1,48 @@
 package org.luogu.part1.ch4.p1047;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int L = in.nextInt() + 1;
+        int M = in.nextInt();
+        LinkedList<int[]> subway = new LinkedList<>();
+        for (int i = 0; i < M; i++) {
+            int lo = in.nextInt(), hi = in.nextInt();
+            if (subway.isEmpty() || lo > subway.getLast()[1]) {
+                subway.add(new int[]{lo, hi});
+                continue;
+            }
+            int loIndex = 0, hiIndex = subway.size() - 1;
+            int index = 0;
+            while (index < subway.size()) {
+                int[] interval = subway.get(index);
+                if (lo <= interval[1]) {
+                    if (lo < interval[0]) {
+                        interval[0] = lo;
+                    }
+                    break;
+                }
+                index++;
+            }
+            while (index < subway.size()) {
+                int[] interval = subway.get(index);
+                if (hi < interval[0]) {
+                    if (hi > subway.get(index - 1)[1]) {
+                        subway.get(index - 1)[1] = hi;
+                        break;
+                    }
+                }
+                index++;
+            }
+        }
+        for (int[] interval : subway) {
+            L -= interval[1] - interval[0] + 1;
+        }
+        System.out.println(L);
+    }
 }
