247977a3099d862cf0d1d2b17dc40228d0755aab
TangBean <984354469@qq.com>
2019-04-12 08:01:27 +0800

    luogu-p1-ch4 P1047 校门外的树_update1

--- src/main/java/org/luogu/part1/ch4/p1047/Main.java
+++ src/main/java/org/luogu/part1/ch4/p1047/Main.java
@@ -5,6 +5,12 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * 本质上说，有 3 种可能：
+ * 1. 新加入的区间包含在原来的区间中，无需改变 subway 区间集合
+ * 2. 新加入的区间是一个独立的区间，把它插入 subway 区间即可
+ * 3. 新加入的区间横跨了 subway 区间，subway 区间需要进行区间合并（最难处理！）
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -17,32 +23,32 @@ public class Main {
                 subway.add(new int[]{lo, hi});
                 continue;
             }
-            int loIndex = 0, hiIndex = subway.size() - 1;
-            int index = 0;
-            while (index < subway.size()) {
-                int[] interval = subway.get(index);
-                if (lo <= interval[1]) {
-                    if (lo < interval[0]) {
-                        interval[0] = lo;
-                    }
-                    break;
-                }
-                index++;
+            if (lo >= subway.getLast()[0] && hi > subway.getLast()[1]) {
+                subway.getLast()[1] = hi;
+                continue;
             }
-            while (index < subway.size()) {
-                int[] interval = subway.get(index);
-                if (hi < interval[0]) {
-                    if (hi > subway.get(index - 1)[1]) {
-                        subway.get(index - 1)[1] = hi;
-                        break;
-                    }
-                }
-                index++;
+
+            int j = 0;
+            while (j < subway.size() - 1 && lo < subway.get(j + 1)[0]) {
+                j++;
+            }
+            if (lo > subway.get(j)[1] && hi < subway.get(j + 1)[0]) {
+                subway.add(j + 1, new int[]{lo, hi});
+                continue;
+            }
+            if (hi > subway.get(j)[1]) {
+                subway.get(j++)[1] = hi;
+            }
+            while (j < subway.size() && subway.get(j)[1] <= hi) {
+                subway.remove(j);
+            }
+            if (j != subway.size() && hi >= subway.get(j)[0]) {
+                subway.get(j - 1)[1] = subway.get(j)[1];
+                subway.remove(j);
             }
         }
         for (int[] interval : subway) {
             L -= interval[1] - interval[0] + 1;
         }
-        System.out.println(L);
     }
 }
