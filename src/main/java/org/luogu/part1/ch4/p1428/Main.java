1c0cc42bdd31e69100850ef1d8c7188dd60080ea
TangBean <984354469@qq.com>
2019-04-15 07:55:26 +0800

    luogu-p1-ch4 P1428 小鱼比可爱_finished

--- src/main/java/org/luogu/part1/ch4/p1428/Main.java
+++ src/main/java/org/luogu/part1/ch4/p1428/Main.java
@@ -3,21 +3,31 @@ package org.luogu.part1.ch4.p1428;
 import java.util.Scanner;
 
 /**
- * 逆序对！
+ * 归并排序求逆序对 + 把输入数据用一个类包装一下（因为需要额外记录每个输入数据在输入数组中的位置）
  */
 public class Main {
-    private static int[] cuteNum;
-    private static int[] mergeHelper;
+    private static ValueWithIndex[] cuteNum;
+    private static ValueWithIndex[] mergeHelper;
     private static int[] cuteCount;
+
+    private static class ValueWithIndex {
+        int val;
+        int index;
+
+        ValueWithIndex(int val, int index) {
+            this.val = val;
+            this.index = index;
+        }
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        cuteNum = new int[n];
+        cuteNum = new ValueWithIndex[n];
         for (int i = 0; i < n; i++) {
-            cuteNum[i] = in.nextInt();
+            cuteNum[i] = new ValueWithIndex(in.nextInt(), i);
         }
-        mergeHelper = new int[n];
+        mergeHelper = new ValueWithIndex[n];
         cuteCount = new int[n];
         sort(0, n-1);
         for (int i = 0; i < n; i++) {
@@ -37,19 +47,16 @@ public class Main {
 
     private static void merge(int lo, int mid, int hi) {
         int i = lo, j = mid + 1;
-        for (int k = lo; k <= hi; k++) {
-            mergeHelper[k] = cuteNum[k];
-        }
+        System.arraycopy(cuteNum, lo, mergeHelper, lo, hi + 1 - lo);
         for (int k = lo; k <= hi; k++) {
             if (i > mid) {
                 cuteNum[k] = mergeHelper[j++];
             } else if (j > hi) {
-                cuteCount[i]++;
                 cuteNum[k] = mergeHelper[i++];
-            } else if (mergeHelper[i] <= mergeHelper[j]) {
+            } else if (mergeHelper[i].val >= mergeHelper[j].val) {
                 cuteNum[k] = mergeHelper[i++];
             } else {
-                cuteCount[j]++;
+                cuteCount[mergeHelper[j].index] += mid - i + 1;  // 只有这一处逆序对的数量会发生变化
                 cuteNum[k] = mergeHelper[j++];
             }
         }
