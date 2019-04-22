d276bd91f65637a739167ca397795c8b1b1f5025
TangBean <984354469@qq.com>
2019-04-22 08:06:04 +0800

    luogu-p1-ch6 P1028 数的计算_unfinished

--- src/main/java/org/luogu/part1/ch6/p1028/Main.java
+++ src/main/java/org/luogu/part1/ch6/p1028/Main.java
@@ -1,11 +1,22 @@
 package org.luogu.part1.ch6.p1028;
 
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
+    static int res = 1;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String n = in.next();
+        numCount(Integer.parseInt(n.substring(0, 1)));
+        System.out.println(res);
+    }
 
+    private static void numCount(int n) {
+        int curCount = n / 2;
+        res += curCount;
+        for (int i = 1; i <= curCount; i++) {
+            numCount(i);
+        }
     }
 }
