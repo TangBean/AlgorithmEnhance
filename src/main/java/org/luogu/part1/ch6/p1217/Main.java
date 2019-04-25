a4d1f211ddb99988750d8b1962a69290133a6ec6
TangBean <984354469@qq.com>
2019-04-26 07:23:22 +0800

    luogu-p1-ch6 P1217 [USACO1.5]回文质数 Prime Palindromes

--- src/main/java/org/luogu/part1/ch6/p1217/Main.java
+++ src/main/java/org/luogu/part1/ch6/p1217/Main.java
@@ -7,6 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt(), b = in.nextInt();
+        /*
+        偶数位数的回文数，除了 11 之外，一定不是质数。
+        使用这种方法可以将搜索范围缩小 10 倍，可以防止 TLE。
+         */
+        b = (b > 9999999) ? 9999999 : b;
+
         boolean[] prime = getPrimeList(a, b);
         for (int i = a; i <= b; i++) {
             if (prime[i - a] && isPalindromes(i)) {
@@ -28,6 +34,14 @@ public class Main {
                 for (int j = i << 1; j <= end; j += i) {
                     helper[j] = false;
                 }
+                /*
+                j 开始的位置：Math.max(2, (a + i - 1) / i) * i 很关键！
+                为什么要 (a + i - 1) / i < 2 时要取 2 呢？
+                因为我们使用埃氏筛法的过程是：
+                1. 找到当前区间中最小的质数
+                2. 把这个质数的 2，3，4 ... 以上的倍数划去
+                可以看到，是从 2 倍开始的，所以要和 2 取一下 max。
+                 */
                 for (int j = Math.max(2, (a + i - 1) / i) * i; j <= b; j += i) {
                     prime[j - a] = false;
                 }
