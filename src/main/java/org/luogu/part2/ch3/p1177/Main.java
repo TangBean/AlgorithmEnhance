6ed3547972ee55a4d7ad08a9b668793ec177de49
TangBean <984354469@qq.com>
2019-05-09 22:58:24 +0800

    luogu-p2-ch3 P1177 【模板】快速排序_finished

--- src/main/java/org/luogu/part2/ch3/p1177/Main.java
+++ src/main/java/org/luogu/part2/ch3/p1177/Main.java
@@ -3,6 +3,9 @@ package org.luogu.part2.ch3.p1177;
 import java.util.Scanner;
 import java.util.Stack;
 
+/**
+ * 不使用递归的方法实现快速排序，递归方法在 N 较大时，比如 N = 10000 时，会栈溢出
+ */
 public class Main {
     private static Stack<Integer> stack = new Stack<>();
 
@@ -22,27 +25,30 @@ public class Main {
     }
 
     private static void partitionByLo(int[] nums) {
-        if (stack.isEmpty()) {
-            return;
+        while (!stack.isEmpty()) {
+            int lo = stack.pop(), hi = stack.pop();
+            int i = lo, j = hi;
+            while (i < j) {
+                while (i < j && nums[j] >= nums[lo]) {
+                    j--;
+                }
+                while (i < j && nums[i] <= nums[lo]) {
+                    i++;
+                }
+                if (i < j) {
+                    swap(nums, i, j);
+                }
+            }
+            swap(nums, lo, i);
+            if (i + 1 < hi) {
+                stack.push(hi);
+                stack.push(i + 1);
+            }
+            if (lo < i - 1) {
+                stack.push(i - 1);
+                stack.push(lo);
+            }
         }
-        int lo = stack.pop(), hi = stack.pop();
-        int i = lo, j = hi;
-        while (i < j) {
-            while (i < j && nums[j] >= nums[lo]) {
-                j--;
-            }
-            while (i < j && nums[i] <= nums[lo]) {
-                i++;
-            }
-            if (i < j) {
-                swap(nums, i, j);
-            }
-        }
-        swap(nums, lo, i);
-        stack.push(hi);
-        stack.push(i + 1);
-        stack.push(lo);
-        stack.push(i - 1);
     }
 
     private static void swap(int[] nums, int a, int b) {
