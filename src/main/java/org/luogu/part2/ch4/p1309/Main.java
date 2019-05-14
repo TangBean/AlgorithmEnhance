2fafccfa139900f60780580a535362101d5ad7b3
TangBean <984354469@qq.com>
2019-05-14 08:04:11 +0800

    luogu-p2-ch4 P1309 瑞士轮_optimize1_unfinished

--- src/main/java/org/luogu/part2/ch4/p1309/Main.java
+++ src/main/java/org/luogu/part2/ch4/p1309/Main.java
@@ -1,9 +1,10 @@
 package org.luogu.part2.ch4.p1309;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    private static Player[] tmp;
+
     private static class Player implements Comparable<Player> {
         int no_;
         int score;
@@ -24,25 +25,53 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt(), R = in.nextInt(), Q = in.nextInt();
-        Player[] players = new Player[2*N];
-        for (int i = 1; i <= 2*N; i++) {
+        Player[] players = new Player[2 * N];
+        tmp = new Player[2 * N];
+        for (int i = 1; i <= 2 * N; i++) {
             players[i - 1] = new Player(i, in.nextInt(), 0);
         }
         for (int i = 0; i < 2 * N; i++) {
             players[i].weight = in.nextInt();
         }
-        Arrays.sort(players);
+        mergeSort(players, 0, 2 * N - 1);
 
         while (R-- > 0) {
-            for (int i = 0; i < N; i++) {
-                if (players[i].weight > players[N + i].weight) {
+            for (int i = 0; i < 2 * N; i += 2) {
+                if (players[i].weight > players[i + 1].weight) {
                     players[i].score++;
                 } else {
-                    players[N + i].score++;
+                    players[i + 1].score++;
                 }
             }
-            Arrays.sort(players);
+            mergeSort(players, 0, 2 * N - 1);
         }
         System.out.println(players[Q - 1].no_);
+    }
+
+    private static void mergeSort(Player[] players, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        mergeSort(players, lo, mid);
+        mergeSort(players, mid + 1, hi);
+        merge(players, lo, mid, hi);
+    }
+
+    private static void merge(Player[] players, int lo, int mid, int hi) {
+        int len = hi - lo + 1;
+        System.arraycopy(players, lo, tmp, lo, len);
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                players[k] = tmp[j++];
+            } else if (j > hi) {
+                players[k] = tmp[i++];
+            } else if (tmp[i].compareTo(tmp[j]) < 0) {
+                players[k] = tmp[i++];
+            } else {
+                players[k] = tmp[j++];
+            }
+        }
     }
 }
