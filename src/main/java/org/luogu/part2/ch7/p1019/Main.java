9ccc2ca633930f0946188ef29ad6725e1e28e538
TangBean <984354469@qq.com>
2019-05-25 08:06:42 +0800

    luogu-p2-ch7 P1019 单词接龙_unfinished_2

--- src/main/java/org/luogu/part2/ch7/p1019/Main.java
+++ src/main/java/org/luogu/part2/ch7/p1019/Main.java
@@ -17,33 +17,40 @@ public class Main {
         int[][] overlap = new int[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (i == j) {
-                    overlap[i][j] = 0;
-                } else {
-                    overlap[i][j] = getOverlap(words[i], words[j]);
-                }
+                overlap[i][j] = getOverlap(words[i], words[j]);
             }
         }
 
         int[] visited = new int[N];
         for (int i = 0; i < N; i++) {
             if (words[i].startsWith(start)) {
-
+                visited[i]++;
+                maxRes = Math.max(maxRes, words[i].length());
+                dfs(words, i, words[i].length(), N, visited, overlap);
+                visited[i]--;
             }
         }
         System.out.println(maxRes);
     }
 
-    private static void dfs(String[] words, int cur, int N, int[] visited, int[][] overlap) {
+    private static void dfs(String[] words, int cur, int curLen, int N, int[] visited, int[][] overlap) {
         for (int i = 0; i < N; i++) {
-
+            if (overlap[cur][i] > 0 && visited[i] < 2) {
+                int lenAdd = words[i].length() - overlap[cur][i];
+                curLen += lenAdd;
+                visited[i]++;
+                maxRes = Math.max(maxRes, curLen);
+                dfs(words, i, curLen, N, visited, overlap);
+                curLen -= lenAdd;
+                visited[i]--;
+            }
         }
     }
 
     private static int getOverlap(String left, String right) {
         int maxLen = Math.min(left.length(), right.length());
         int Nl = left.length(), Nr = right.length();
-        if (left.substring(Nl - maxLen, Nl).equals(right.substring(0, maxLen))) {
+        if (!left.equals(right) && left.substring(Nl - maxLen, Nl).equals(right.substring(0, maxLen))) {
             return 0;
         }
         maxLen--;
